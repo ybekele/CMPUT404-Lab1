3ac830e2e581211dccf61a824572a6d7158e7496
ybekele <yonael@ualberta.ca>
2019-01-09 18:14:53 -0700

    modified lab1.py to call itself

--- lab1.py
+++ lab1.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python
+
 import requests 
 
 r = requests.get("http://www.google.com")
 print(r.status_code)
 print(r.text)
 
+
+a = requests.get("https://raw.githubusercontent.com/ybekele/CMPUT404-Lab1/master/lab1.py")
+print(a.text)
