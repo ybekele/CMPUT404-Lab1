c79b1b16771c11884137765aa10bad31101b2e24
ybekele <yonael@ualberta.ca>
2019-01-10 19:07:46 -0700

    adding the 2 response text from hindle's site using curl and the curl with the post request. Also a diff file comparing the two,

--- lab1.py
+++ lab1.py
@@ -5,7 +5,7 @@ import requests
 r = requests.get("http://www.google.com")
 print(r.status_code)
 print(r.text)
-
-
+#print("version = " )
+#print(requests.__version__)
 a = requests.get("https://raw.githubusercontent.com/ybekele/CMPUT404-Lab1/master/lab1.py")
 print(a.text)
